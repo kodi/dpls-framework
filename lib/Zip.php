c6b6f6b2fac79ba9180572c1fc81f57de5eed2ed
Dragan Bajcic <dragan.bajcic@gmail.com>
2009-03-03 13:43:30 +0100

    seccond commit, now with helper files

--- lib/Zip.php
+++ lib/Zip.php
@@ -1,34 +1,10 @@
 <?php  
-/**
- * CodeIgniter
- *
- * An open source application development framework for PHP 4.3.2 or newer
- *
- * @package		CodeIgniter
- * @author		ExpressionEngine Dev Team
- * @copyright	Copyright (c) 2008, EllisLab, Inc.
- * @license		http://codeigniter.com/user_guide/license.html
- * @link		http://codeigniter.com
- * @since		Version 1.0
- * @filesource
- */
-
-// ------------------------------------------------------------------------
 
 /**
  * Zip Compression Class
  *
- * This class is based on a library I found at Zend:
- * http://www.zend.com/codex.php?id=696&single=1
- *
- * The original library is a little rough around the edges so I
- * refactored it and added several additional methods -- Rick Ellis
- *
- * @package		CodeIgniter
- * @subpackage	Libraries
- * @category	Encryption
- * @author		ExpressionEngine Dev Team
- * @link		http://codeigniter.com/user_guide/libraries/zip.html
+ *	Based on Code Igniter ZIP Library 
+ * 	http://codeigniter.com/user_guide/libraries/zip.html
  */
 class DPLS_Zip  {
 
@@ -411,9 +387,6 @@ class DPLS_Zip  {
 			exit($data);
 		}
 
-
-
-	
 	
 	
 }
